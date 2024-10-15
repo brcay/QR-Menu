9dcf039d9a8dc09c152283bce559ac8208a809ed
brcay <brc.ay@hotmail.com>
2024-10-16 01:10:49 +0300

    first commit

---
+++ scripts/ui.js
@@ -0,0 +1,39 @@
+//! Arayüzü etkileyecek tüm fonksiyonlar burada...
+
+const menuList = document.getElementById("menu-list");
+
+export const renderCards = (data) => {
+
+  const cardsHTML = data
+    .map(
+      (item) => `
+      <a
+        href="/detail.html?id=${item.id}"
+        id="card"
+        class="d-flex flex-column flex-md-row text-dark gap-3 text-decoration-none"
+      >
+        <img
+          class="rounded shadow img-fluid"
+          src="${item.img}"
+        />
+
+        <div>
+          <div class="d-flex justify-content-between">
+            <h5>${item.title}</h5>
+            <p class="text-success fw-bold">${(item.price * 30).toFixed(
+              2
+            )}₺</p>
+          </div>
+
+          <p class="lead">
+          ${item.desc}
+          </p>
+        </div>
+      </a>  
+  `
+    )
+    .join("");
+
+  
+  menuList.innerHTML = cardsHTML;
+};
